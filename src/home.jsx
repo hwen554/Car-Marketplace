8a6f5053769b62a231b04926093385910db68238
Gavin Weng <wengh7930@gmail.com>
2024-11-16 23:36:28 +1300

    Initial commit

---
+++ src/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+import { SignInButton } from '@clerk/clerk-react'
+import Header from './components/ui/Header'
+
+function Home() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
